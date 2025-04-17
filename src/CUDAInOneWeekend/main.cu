88205b4fe20ba20c81b429c87e3888ca09caff9f
Jilin Zheng <jilin@bu.edu>
2025-04-16 23:34:07 -0400

    Update ray-sphere intersection code to Chapter 6.2

--- src/CUDAInOneWeekend/main.cu
+++ src/CUDAInOneWeekend/main.cu
@@ -130,17 +130,26 @@ void initialize_camera(int h_image_width, int h_image_height) {
 }
 */
 
-__device__ bool hit_sphere(const point3& center, float radius, const ray& r) {
+__device__ float hit_sphere(const point3& center, float radius, const ray& r) {
     vec3 oc = center - r.origin();
-    float a = dot(r.direction(), r.direction());
-    float b = -2.0 * dot(r.direction(), oc);
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std::sqrt(discriminant)) / a;
+    }
 }
 
 __device__ color ray_color(const ray& r) {
-    if (hit_sphere(point3(0,0,-1), 0.5, r)) return color(1, 0, 0);
+    float t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
